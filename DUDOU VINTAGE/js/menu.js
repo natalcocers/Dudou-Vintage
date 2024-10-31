a3a0bf34134dacbcda49af6f79a19c82d0697af8
natalcocers <natalcocers@gmail.com>
2024-10-31 10:35:14 -0300

    Delete DUDOU VINTAGE directory

--- DUDOU VINTAGE/js/menu.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("hamburger").addEventListener("click", function() {
-    this.classList.toggle("active");
-    document.getElementById("menu").classList.toggle("active");
-});
