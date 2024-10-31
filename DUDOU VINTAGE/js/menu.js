3d5ce5ccfa4383aa14577839424ac7bdd1062dcc
natalcocers <natalcocers@gmail.com>
2024-10-31 10:24:10 -0300

    Add files via upload

---
+++ DUDOU VINTAGE/js/menu.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", function() {
+    this.classList.toggle("active");
+    document.getElementById("menu").classList.toggle("active");
+});
